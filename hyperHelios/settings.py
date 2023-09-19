5d399660cd295423adeb54b662f910c013c55530
salahuddin8544 <salahuddinahmed8544@gmail.com>
2023-09-19 12:32:23 +0600

    Form design completed

--- hyperHelios/settings.py
+++ hyperHelios/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'hyperHelios.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [BASE_DIR / 'static']
--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from .views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('project.urls')),
+    path('',index),
 ]
